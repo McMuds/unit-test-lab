dad432d38c24bebc8d7ee07f6e299ab01365fe2f
user <user@users-MacBook-Pro.local>
2023-03-01 16:05:53 +0000

    added a few tests

--- functions_lab_1_start_point/src/python_functions_practice.py
+++ functions_lab_1_start_point/src/python_functions_practice.py
@@ -3,3 +3,15 @@ def return_10():
 
 def add(num1, num2):
     return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def multiply(num1, num2):
+    return num1 * num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+def length_of_string(string):
+    return len(string)
